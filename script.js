c94ef931e85520e4d6a19c1e0654076f82c5f1bf
Rafael Perger <rafael.perger@gmail.com>
2022-03-03 07:56:16 +0100

    Implement displayContent method in gameBoard module.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const gameBoard = (() => {
     let gridArray = ["X", "O", "X", 
                     "X", "X", "O", 
-                    "O", "O", "X"]
+                    "O", "O", "X"];
+    
+    const gameBoardDiv = document.querySelector(".game-board");
+
+    function createGridCell(item) {
+        const xoContainer = document.createElement("div");
+        xoContainer.textContent = item;
+
+        gameBoardDiv.appendChild(xoContainer);
+    }
+
+    function displayContent () {
+        gridArray.forEach(createGridCell)
+    }
+
+    return {displayContent};
 })();
 
 const Player = (name) => {
-    const name = name;
+    const playeName = name;
     let score = 0;
 }
+
+gameBoard.displayContent();
