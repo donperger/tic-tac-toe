20a14d001b0d861601e0491890301d0d797e9064
Rafael Perger <rafael.perger@gmail.com>
2022-03-03 07:42:03 +0100

    Create gameBoard module and Player factory function.

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameBoard = (() => {
+    let gridArray = ["X", "O", "X", 
+                    "X", "X", "O", 
+                    "O", "O", "X"]
+})();
+
+const Player = (name) => {
+    const name = name;
+    let score = 0;
+}
