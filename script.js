be9d24bfc0361c3ad5307a01b2df3ccc77142e29
Rafael Perger <rafael.perger@gmail.com>
2022-03-04 18:21:49 +0100

    Create displayScores function.

--- script.js
+++ script.js
@@ -2,31 +2,20 @@ const gameBoard = (() => {
     let gridArray = ["", "", "", 
                     "", "", "", 
                     "", "", ""];
-    
-    const gameBoardDiv = document.querySelector(".game-board");
 
-    function createGridCell(item, index) {
-        const xoContainer = document.createElement("div");
-        xoContainer.setAttribute("id",`filed-${index}`);
-        xoContainer.classList.add("field");
-        xoContainer.textContent = item;
-
-        gameBoardDiv.appendChild(xoContainer);
-    }
-
-    function displayContent () {
-        gridArray.forEach((item, index) => {
-            createGridCell(item, index)
-        })
-    }
-
-    
-
-    return {displayContent};
+    return {gridArray}
 })();
 
 const displayController = (() => {
     let mark = "X";
+
+    function displayScores(scoreP1, scoreP2) {
+        const player1ScoreDiv = document.querySelector(".player1-score");
+        const player2ScoreDiv = document.querySelector(".player2-score");
+
+        player1ScoreDiv.textContent = scoreP1;
+        player2ScoreDiv.textContent = scoreP2;
+    }
 
     function initializeListeners () {
         const gameBoardFields = document.querySelectorAll(".field");
@@ -39,12 +28,27 @@ const displayController = (() => {
     function addText (field) {
         if (!field.innerText) {
             field.innerText = mark;
+            mark = mark === "X" ? "O" : "X";
         }
-        
-        mark = mark === "X" ? "O" : "X";
     }
-    
-    return {initializeListeners}
+        const gameBoardDiv = document.querySelector(".game-board");
+
+    function createGridCell(item, index) {
+        const xoContainer = document.createElement("div");
+        xoContainer.setAttribute("id",`filed-${index}`);
+        xoContainer.classList.add("field");
+        xoContainer.textContent = item;
+
+        gameBoardDiv.appendChild(xoContainer);
+    }
+
+    function displayContent (array) {
+        array.forEach((item, index) => {
+            createGridCell(item, index)
+        })
+    }
+
+    return {initializeListeners, displayContent, displayScores}
 
 })();
 
@@ -57,8 +61,7 @@ const player = (name, mark) => {
 
 const player1 = player("p1", "X");
 const player2 = player("p2", "O");
-console.log(player1);
-console.log(player2);
 
-gameBoard.displayContent();
+displayController.displayScores(player1.score, player2.score);
+displayController.displayContent( gameBoard.gridArray);
 displayController.initializeListeners();
